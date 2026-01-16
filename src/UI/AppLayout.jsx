e7ba81484d0a67759177630da0618d2deea4b511
Saramoawad <saramoawad732@gmail.com>
2026-01-16 21:32:28 +0200

    Initial commit

---
+++ src/UI/AppLayout.jsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import CartOverview from '../Features/Cart/CartOverview'
+import { Outlet } from 'react-router-dom';
+function AppLayout() {
+    return (
+        <div>
+            <Header/>
+            <main>
+                <h1>content</h1>
+                <Outlet/>
+            </main>
+            <CartOverview/>
+        </div>
+    )
+}
+
+export default AppLayout
