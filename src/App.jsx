2881a22290fcc50613b0c4736c1fb6a5a92452c6
Saramoawad <saramoawad732@gmail.com>
2026-01-17 09:39:47 +0200

    feching data with react router loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from './UI/Home'
-import Menu from "./Features/Menu/Menu";
-import Cart from './Features/Cart/Cart'
-import CreateOrder from './Features/Order/CreateOrder';
-import Order from './Features/Order/Order'
+import Home from "./UI/Home";
+import Menu, { loader as menuLoader } from "./Features/Menu/Menu";
+import Cart from "./Features/Cart/Cart";
+import CreateOrder from "./Features/Order/CreateOrder";
+import Order from "./Features/Order/Order";
 import AppLayout from "./UI/AppLayout";
-const router =createBrowserRouter([
+const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
-        
-  {
-    path:'/',
-    element:<Home/>
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
-  {
-    path:'/menu',
-    element:<Menu/>
-  },
-  {
-    path:'/cart',
-    element:<Cart/>
-  },
-  {
-    path:'/order/new',
-    element:<CreateOrder/>
-  },
-  {
-    path:'/order/:orderId',
-    element:<Order/>
-  }
-    ]
-  }
-
-])
+]);
 
 function App() {
-
-
-  return (
-   <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
